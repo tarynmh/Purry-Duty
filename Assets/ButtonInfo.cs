612194b475f64ab36613c103bd0f8012ef4c9899
BuildTools <60895815+RebeccaCoryell@users.noreply.github.com>
2022-04-27 02:17:29 -0600

    added rough shop functionality

---
+++ Assets/ButtonInfo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class ButtonInfo : MonoBehaviour
+{
+    public int ItemID;
+    public TextMeshProUGUI PriceTxt;
+    public TextMeshProUGUI QuantityTxt;
+    public GameObject ShopManager;
+
+    void Start()
+    {
+        Debug.Log("Start" + ItemID);
+        Debug.Log("^" + ItemID + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID]);
+        int[,] items = ShopManager.GetComponent<ShopManagerScript>().shopItems;
+        Debug.Log("Items: " + items[2, 3]);
+
+        PriceTxt.text = "Price: " + ShopManager.GetComponent<ShopManagerScript>().shopItems[2, ItemID].ToString();
+        QuantityTxt.text = "Quantity: " + ShopManager.GetComponent<ShopManagerScript>().shopItems[3, ItemID].ToString();
+    }
+}
